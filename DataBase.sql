ef970a02dec76c3ab76142264ca63a9b9ba4aa98
Daria <d.plewa25@gmail.com>
2023-04-27 18:34:41 +0200

    Creating new files

---
+++ DataBase.sql
@@ -0,0 +1,131 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Apr 27, 2023 at 06:34 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mydb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authority`
+--
+-- Error reading structure for table mydb.authority: #1932 - Table &#039;mydb.authority&#039; doesn&#039;t exist in engine
+-- Error reading data for table mydb.authority: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `mydb`.`authority`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_details`
+--
+-- Error reading structure for table mydb.contact_details: #1932 - Table &#039;mydb.contact_details&#039; doesn&#039;t exist in engine
+-- Error reading data for table mydb.contact_details: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `mydb`.`contact_details`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `equipment`
+--
+-- Error reading structure for table mydb.equipment: #1932 - Table &#039;mydb.equipment&#039; doesn&#039;t exist in engine
+-- Error reading data for table mydb.equipment: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `mydb`.`equipment`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `equipment_methods`
+--
+-- Error reading structure for table mydb.equipment_methods: #1932 - Table &#039;mydb.equipment_methods&#039; doesn&#039;t exist in engine
+-- Error reading data for table mydb.equipment_methods: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `mydb`.`equipment_methods`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `experiments`
+--
+-- Error reading structure for table mydb.experiments: #1932 - Table &#039;mydb.experiments&#039; doesn&#039;t exist in engine
+-- Error reading data for table mydb.experiments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `mydb`.`experiments`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `methods`
+--
+-- Error reading structure for table mydb.methods: #1932 - Table &#039;mydb.methods&#039; doesn&#039;t exist in engine
+-- Error reading data for table mydb.methods: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `mydb`.`methods`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `overview`
+--
+-- Error reading structure for table mydb.overview: #1932 - Table &#039;mydb.overview&#039; doesn&#039;t exist in engine
+-- Error reading data for table mydb.overview: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `mydb`.`overview`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `position_list`
+--
+-- Error reading structure for table mydb.position_list: #1932 - Table &#039;mydb.position_list&#039; doesn&#039;t exist in engine
+-- Error reading data for table mydb.position_list: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `mydb`.`position_list`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projects`
+--
+-- Error reading structure for table mydb.projects: #1932 - Table &#039;mydb.projects&#039; doesn&#039;t exist in engine
+-- Error reading data for table mydb.projects: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `mydb`.`projects`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_workers`
+--
+-- Error reading structure for table mydb.project_workers: #1932 - Table &#039;mydb.project_workers&#039; doesn&#039;t exist in engine
+-- Error reading data for table mydb.project_workers: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `mydb`.`project_workers`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `samples`
+--
+-- Error reading structure for table mydb.samples: #1932 - Table &#039;mydb.samples&#039; doesn&#039;t exist in engine
+-- Error reading data for table mydb.samples: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `mydb`.`samples`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `samples_lista`
+--
+-- Error reading structure for table mydb.samples_lista: #1932 - Table &#039;mydb.samples_lista&#039; doesn&#039;t exist in engine
+-- Error reading data for table mydb.samples_lista: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `mydb`.`samples_lista`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+-- Error reading structure for table mydb.user: #1932 - Table &#039;mydb.user&#039; doesn&#039;t exist in engine
+-- Error reading data for table mydb.user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `mydb`.`user`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
